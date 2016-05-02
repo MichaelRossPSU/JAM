9913cc6ae55cc6e49fe7e91b44398ff79cdf1afc
John Darling <darling.johnt@gmail.com>
2016-05-02 11:03:54 -0400

    Added Final Report

--- SRC/PIRtest.py
+++ SRC/PIRtest.py
@@ -1,6 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
+fromaddr = 'blackflamingo@ymail.com'
+toaddrs  = 'blackflamingo@ymail.com'
+msg = 'The motion sensor has been tripped. There may be an intruder'
+
+
+# Credentials (if needed)
+username = 'blackflamingo@ymail.com'
+password = 'Losalamos679'
 
 sensor = 17
 
@@ -26,3 +34,9 @@ while True:
     	pwm.start(50)
     	GPIO.output(18, True)
     	time.sleep(30)
+        server = smtplib.SMTP("smtp.mail.yahoo.com",587)
+        server.ehlo()
+        server.starttls()
+        server.login(username,password)
+        server.sendmail(fromaddr, toaddrs, msg)
+        server.quit()
